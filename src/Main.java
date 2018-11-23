11fccdb9d76bffbe14a1484ea4e00373bd63d85c
snoul <aspire@op.pl>
2018-11-23 23:46:37 +0100

    odkomentowanie

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-//        ReadFromOCR r1=new ReadFromOCR();
-//        r1.test();
-//
-//        AccountValidation ac1 = new AccountValidation();
-//        ac1.validation();
+        ReadFromOCR r1=new ReadFromOCR();
+        r1.test();
+
+        AccountValidation ac1 = new AccountValidation();
+        ac1.validation();
 
 
         //   poprawia numer konta
-        String numer = "123456788";
-        System.out.println("Suma kontrolna poprawna? : " + AccountNumber.checkSum(numer));
-        System.out.println("Poprawiony nr konta: " + ErrorCorrection.guessNumber(numer));
+//        String numer = "123456788";
+//        System.out.println("Suma kontrolna poprawna? : " + AccountNumber.checkSum(numer));
+//        System.out.println("Poprawiony nr konta: " + ErrorCorrection.guessNumber(numer));
 
 
     }}
